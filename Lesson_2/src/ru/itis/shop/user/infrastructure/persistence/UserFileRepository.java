20203ca400ad9f24b7757fe556d3ce6168d9d070
Глеб Самарин <glebsamarin@MacBook-Air-Gleb.local>
2026-07-08 01:42:29 +0700

    добавлен метод updateProfile

---
+++ Lesson_2/src/ru/itis/shop/user/infrastructure/persistence/UserFileRepository.java
@@ -0,0 +1,119 @@
+package ru.itis.shop.user.infrastructure.persistence;
+
+import ru.itis.shop.user.domain.User;
+import ru.itis.shop.user.repository.UserRepository;
+
+import java.io.*;
+import java.util.Optional;
+import java.util.UUID;
+
+public class UserFileRepository implements UserRepository {
+
+    private final String fileName;
+
+    private final UserMapper userMapper;
+
+    public UserFileRepository(String fileName, UserMapper userMapper) {
+        this.fileName = fileName;
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public void save(User user) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
+            String id = UUID.randomUUID().toString();
+            user.setId(id);
+            writer.write(userMapper.toLine(user));
+            writer.newLine();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+
+            String line = reader.readLine();
+
+            while (line != null) {
+
+                User user = userMapper.fromLine(line);
+
+                if (user.getEmail().equals(email)) {
+                    return Optional.of(user);
+                }
+
+                line = reader.readLine();
+            }
+
+            return Optional.empty();
+
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public Optional<User> findById(String id) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+
+            String line = reader.readLine();
+
+            while (line != null) {
+
+                User user = userMapper.fromLine(line);
+
+                if (user.getId().equals(id)) {
+                    return Optional.of(user);
+                }
+
+                line = reader.readLine();
+            }
+
+            return Optional.empty();
+
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void update(User user){
+        try{
+            File inputFile = new File(fileName);
+            File tempFile = new File("temp.txt");
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+                 BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile))) {
+
+                String line = reader.readLine();
+
+                while (line != null) {
+
+                    User existingUser = userMapper.fromLine(line);
+
+                    if (existingUser.getId().equals(user.getId())) {
+                        writer.write(userMapper.toLine(user));
+                    } else {
+                        writer.write(line);
+                    }
+
+                    writer.newLine();
+                    line = reader.readLine();
+                }
+            }
+
+            if (!inputFile.delete()) {
+                throw new IllegalStateException("Could not delete original file");
+            }
+
+            if (!tempFile.renameTo(inputFile)) {
+                throw new IllegalStateException("Could not rename temp file");
+            }
+
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
